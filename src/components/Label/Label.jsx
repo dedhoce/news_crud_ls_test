91ffeec4c999dc9c2763c86e010ea9ba89cd8b15
A.N.Melitskov <dedhoce@yandex.ru>
2024-10-06 16:23:42 +0300

    done components

---
+++ src/components/Label/Label.jsx
@@ -0,0 +1,32 @@
+function Label({ valueData, name, value, error, onChange }) {
+    console.log(name, error)
+    return (
+        <>
+            <label>
+                {name[0].toUpperCase() + name.slice(1) + ':'}
+                {name === "title" ?
+                    <input
+                        name={name}
+                        type="text"
+                        placeholder={!valueData ? `enter new ${name}` : 'edit'}
+                        onChange={onChange}
+                        value={value ? value : ''}
+                        minLength='2'
+                        required />
+                    :
+                    <textarea
+                        name={name}
+                        type="text"
+                        placeholder={!valueData ? `enter new ${name}` : 'edit'}
+                        onChange={onChange}
+                        value={value ? value : ''}
+                        minLength='2'
+                        required />}
+            </label>
+            {error && <span className='error'>{error}</span>}
+        </>
+
+    );
+}
+
+export default Label;
